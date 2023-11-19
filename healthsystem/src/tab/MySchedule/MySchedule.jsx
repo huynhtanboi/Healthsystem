9eb022a047a64609b06167901144ab2957de5f64
thanhtaita <taita.studymail@gmail.com>
2023-11-19 10:20:19 -0600

    done

--- healthsystem/src/tab/MySchedule/MySchedule.jsx
+++ healthsystem/src/tab/MySchedule/MySchedule.jsx
@@ -1,16 +1,15 @@
 import "./MySchedule.css";
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 const MyShedule = () => {
-
-  onst [timeSlots, setTimeSlots] = useState([]);
+  const [timeSlots, setTimeSlots] = useState([]);
   const [selectedSlot, setSelectedSlot] = useState(null);
   const [selectedVaccine, setSelectedVaccine] = useState(null);
 
   useEffect(() => {
     // Fetch available time slots from the backend
-    fetch('http://localhost:3001/timeslots')
-      .then(response => response.json())
-      .then(data => setTimeSlots(data));
+    fetch("http://localhost:3001/timeslots")
+      .then((response) => response.json())
+      .then((data) => setTimeSlots(data));
   }, []);
 
   const handleSlotSelection = (slot) => {
@@ -20,25 +19,28 @@ const MyShedule = () => {
     setSelectedVaccine(vaccine);
 
     // Send the selected slot and vaccine to the backend
-    fetch('http://localhost:3600/record', {//placehold wait for record and fix later
-      method: 'POST',
+    fetch("http://localhost:3600/record", {
+      //placehold wait for record and fix later
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ slot: selectedSlot, vaccine: vaccine }),
     })
-      .then(response => response.json())
-      .then(data => console.log(data)); // Handle the response from the backend
+      .then((response) => response.json())
+      .then((data) => console.log(data)); // Handle the response from the backend
   };
 
-  rereturn (
+  return (
     <div className="schedule">
-    <div className="schedule-board">
-      <h2>MyShedule</h2>
-      {timeSlots.map(slot => (
+      <div className="schedule-board">
+        <h2>MyShedule</h2>
+        {timeSlots.map((slot) => (
           <div
             key={slot.id}
-            className={`time-slot ${selectedSlot === slot.id ? 'selected' : ''}`}
+            className={`time-slot ${
+              selectedSlot === slot.id ? "selected" : ""
+            }`}
             onClick={() => handleSlotSelection(slot.id)}
           >
             {slot.time}
@@ -49,20 +51,26 @@ const MyShedule = () => {
       <div className="vaccine-board">
         <h2>Choose Your Vaccine</h2>
         <button
-          className={`vaccine-btn ${selectedVaccine === 'Vaccine A' ? 'selected' : ''}`}
-          onClick={() => handleVaccineSelection('Vaccine A')}
+          className={`vaccine-btn ${
+            selectedVaccine === "Vaccine A" ? "selected" : ""
+          }`}
+          onClick={() => handleVaccineSelection("Vaccine A")}
         >
           Vaccine A
         </button>
         <button
-          className={`vaccine-btn ${selectedVaccine === 'Vaccine B' ? 'selected' : ''}`}
-          onClick={() => handleVaccineSelection('Vaccine B')}
+          className={`vaccine-btn ${
+            selectedVaccine === "Vaccine B" ? "selected" : ""
+          }`}
+          onClick={() => handleVaccineSelection("Vaccine B")}
         >
           Vaccine B
         </button>
         <button
-          className={`vaccine-btn ${selectedVaccine === 'Vaccine C' ? 'selected' : ''}`}
-          onClick={() => handleVaccineSelection('Vaccine C')}
+          className={`vaccine-btn ${
+            selectedVaccine === "Vaccine C" ? "selected" : ""
+          }`}
+          onClick={() => handleVaccineSelection("Vaccine C")}
         >
           Vaccine C
         </button>
